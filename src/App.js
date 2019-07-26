507d841e3ff88dcb67970cbac4d2b7055d2d171d
madhusudhan518 <pvenkatamadhusudhanarao@gmail.com>
2019-07-26 18:57:38 +0530

    added folders to make more understandable

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Explorer from './employee/explorer.js';
+import Explorer from './modules/employee/screens/explorer.js';
 
 function App() {
   return (
     <div className="App">
-      <!-- Employee Explorer Screen-->
       <Explorer/>
     </div>
   );
