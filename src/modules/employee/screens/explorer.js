090971bf1bae107991e69ebd908f6a60c6a352f4
madhusudhan518 <pvenkatamadhusudhanarao@gmail.com>
2019-07-27 11:11:29 +0530

    added history of search on screen

--- src/modules/employee/screens/explorer.js
+++ src/modules/employee/screens/explorer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Link } from 'react-router-dom';
-import { Row, Col, Form, Button } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button, ListGroup } from 'react-bootstrap';
 import { fetchEmployee } from '../actions';
 
 class Explorer extends Component {
@@ -25,36 +25,56 @@ class Explorer extends Component {
     const {employeeName} = this.state;
     const { employees } = this.props;
     return (
-      <Row>
-        <Row style={{marginBottom: "20px"}}>
-          <Col md={{ span: 6, offset: 4 }} sm={{ span: 6, offset: 4 }} xs={{ span: 6, offset: 4 }}>
-            <h2>Employee Explorer</h2>
-          </Col>
-        </Row>
-        <Col md={{offset: 3, span: 9}} xs={{offset: 3, span: 9}}>
-          <Form onSubmit={ this.handleSubmit }>
-            <Form.Row>
-              <Col>
-                <Form.Control placeholder="Enter Employee Name" id="EmployeeName" />
-              </Col>
-              <Col>
-                <Button type="submit">Search</Button>
-              </Col>
-            </Form.Row>
-          </Form>
-        </Col>
-          <Col md={{offset: 3, span: 6}}>
-            {employeeName && employees[employeeName.toLowerCase()] ?
-                <h2>
-                  <Link to={`/overview/${employeeName}`}>
-                    Please click here to overview {employeeName}
-                  </Link>
-                </h2>
-              :
-              (employeeName && !employees[employeeName.toLowerCase()] && <h2> Employee Not Found </h2>)
-            }
-          </Col>
-      </Row>
+      <div>
+        <Container>
+          <Row className="justify-content-md-center">
+            <Col xs="auto" md="auto" lg="auto">
+              <h2>Employee Explorer</h2>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={{offset: 3, span: 9}} xs={{offset: 3, span: 9}}>
+              <Form onSubmit={ this.handleSubmit }>
+                <Form.Row>
+                  <Col>
+                    <Form.Control placeholder="Enter Employee Name" id="EmployeeName" />
+                  </Col>
+                  <Col>
+                    <Button type="submit">Search</Button>
+                  </Col>
+                </Form.Row>
+              </Form>
+            </Col>
+          </Row>
+          <Row>
+          <Col xs={4} md={4} lg={3}></Col>
+            <Col xs={6} md={6} lg={8} style={{textAlign: "left"}}>
+              {employeeName && employees[employeeName.toLowerCase()] ?
+                  <h2>
+                    <Link to={`/overview/${employeeName}`}>
+                      Please click here to overview {employeeName}
+                    </Link>
+                  </h2>
+                :
+                (employeeName && !employees[employeeName.toLowerCase()] && <h2> Employee Not Found </h2>)
+              }
+            </Col>
+          </Row>
+        </Container>
+        <br />
+        <Container>
+          <Row>
+            <Col sm={4} md={4} lg={4} >
+              <h4>History of search</h4>
+            </Col>
+            <Col sm={4} md={4} lg={4} >
+              <ListGroup>
+                <ListGroup.Item></ListGroup.Item>
+              </ListGroup>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     )
   }
 }
